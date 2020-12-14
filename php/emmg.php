2cab654fe074772eb5ed7695b8f5345f83cd7299
Marco Wabbel <mw@alogis.com>
2020-12-14 13:45:17 +0100

    ecmg + emmg ready to publish

--- php/emmg.php
+++ php/emmg.php
@@ -12,57 +12,38 @@ $address = $config['muxaddress'];
 /* Einen TCP/IP-Socket erzeugen. */
 $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
 if ($socket === false) {
-    ecco( "socket_create() fehlgeschlagen: Grund: " . socket_strerror(socket_last_error()) . "\n" );
+    ecco( "socket_create() Error: Reason: " . socket_strerror(socket_last_error()) . "\n" );
 	exit;
 } else {
     ecco( "OK.\n" );
 }
 
-ecco( "Versuche, zu '$address' auf Port '$service_port' zu verbinden ..." );
+ecco( "Trying to connect '$address' at Port '$service_port' ..." );
 $result = socket_connect($socket, $address, $service_port);
 if ($result === false) {
-    ecco( "socket_connect() fehlgeschlagen.\nGrund: ($result) " . socket_strerror(socket_last_error($socket)) . "\n");
+    ecco( "socket_connect() Error.\nReason: ($result) " . socket_strerror(socket_last_error($socket)) . "\n");
 	exit;
 } else {
     ecco( "OK.\n");
 }
 
-$in = '';
-$out = '';
 
-    /* SEND UNIQUE EMM TO ALL CARDS IN SUBSCRIPTION */
-    mysql_query(con, "
-		SELECT abo.ppua, abo.`bos`, abo.`eos`,abo.acc, providers.providername, cards.ppsa, providers.chid FROM neovision.providers 
-		join neovision.abo 
-		join neovision.cards \
-        ON providers.chid = abo.chid and cards.ppua = abo.ppua and cards.deleted = 0;
-		"));
+readkeys();
+emm_setup($socket);
+$timer = time()+900;
 
-	/* SEND SHARED EMM TO ALL CARDS IN SUBSCRIPTION */
-    mysql_query(con, "SELECT DISTINCT ppsa FROM neovision.cards JOIN neovision.abo WHERE cards.deleted = 0 AND cards.ppua = abo.ppua;")
-
-    /* DELETE OLD SUBSCRIPTIONS */
-    mysql_query(con, "DELETE FROM neovision.abo WHERE abo.eos < (NOW() - INTERVAL 2 MONTH);")
-
-		
-	/* UPDATE ECM-KEYS */
-    mysql_query(con, "UPDATE neovision.ecmg_keys SET ecmg_keys.ecmkey = md5(rand()*1001), ecmg_keys.modified = (NOW() + INTERVAL 1 MONTH)
-                    WHERE ecmg_keys.modified < (NOW() - INTERVAL 1 MONTH);")
-
-	/* READ SYSTEMKEY */
-	mysql_query(con, "SELECT systemkey FROM emmg_systemkey where id=1");
+while(1==1){
 	
-	mysql_query(con, "SELECT ecmkey FROM ecmg_keys where id=20");
-	mysql_query(con, "SELECT ecmkey FROM ecmg_keys where id=21");
-
-ecco( "send to MUX ...");
-socket_write($socket, $in, strlen($in));
-ecco( "OK.\n");
-
-ecco( "got from MUX:\n\n");
-while ($out = socket_read($socket, 2048)) {
-    ecco( $out);
-}
+	emm_unique($socket);
+	emm_shared($socket);
+	
+	if($timer < time()){
+		dbclean();
+		readkeys();
+	};
+	
+	sleep(120);
+};
 
 ecco( "Close socket...");
 socket_close($socket);
--- php/functions.php
+++ php/functions.php
@@ -24,17 +24,22 @@ function readkeys(){
 	if (!$conn) {
 		die("Connection failed: " . mysqli_connect_error());
 	}
-	//echo "Connected successfully";
 
-	if(date('n')%2){
-		$sql = "SELECT ecmkey FROM ecmg_keys where id = 21";
-	} else {
-		$sql = "SELECT ecmkey FROM ecmg_keys where id = 20";
-	};
-
+	$sql = "SELECT ecmkey FROM ecmg_keys where id = 20";
 	$result = $conn->query($sql);
 	$row = $result->fetch_assoc();
-	$GLOBALS['key'] = hex2bin($row['ecmkey']);
+	$GLOBALS['key20'] = hex2bin($row['ecmkey']);
+
+	$sql = "SELECT ecmkey FROM ecmg_keys where id = 21";
+	$result = $conn->query($sql);
+	$row = $result->fetch_assoc();
+	$GLOBALS['key21'] = hex2bin($row['ecmkey']);
+	
+	$sql = "SELECT systemkey FROM emmg_systemkey where id=1";
+	$result = $conn->query($sql);
+	$row = $result->fetch_assoc();
+	$GLOBALS['syskey'] = hex2bin($row['systemkey']);
+
 	mysqli_close($conn);
 	
 };
@@ -46,6 +51,7 @@ function aes_cmac(&$in,&$key){
 };
 
 function hexdump(&$in){
+	$val = '';
 	for($i=0;$i < strlen($in);$i++){
 		$val .= bin2hex($in[$i]).' ';
 			if($i % 16 == 15){
@@ -56,9 +62,37 @@ function hexdump(&$in){
 	//return bin2hex($in)."\n";
 };
 
-function aes_cbc(&$in){
-	global $key;
-	$key = $GLOBALS['key'];
+function predec(&$in){
+	$in = dechex($in);
+	$out = '';
+	for($i=0;$i<8-strlen($in);$i++){
+		$out .= "0";
+	};
+	return $out.$in;
+};
+
+function prehex(&$in){
+	//$in = dechex($in);
+	$out = '';
+	for($i=0;$i<8-strlen($in);$i++){
+		$out .= "0";
+	};
+	return $out.$in;
+};
+
+function padname(&$in){
+	$len = strlen($in);
+	while($len < 15){
+		$in .= ' ';
+		$len++;
+	};
+	return $in;
+};
+
+
+function aes_cbc(&$in,&$key){
+	//global $key;
+	//$key = $GLOBALS['key'];
 	$iv = hex2bin('00000000000000000000000000000000');
 	$cipher="AES-128-CBC";
 	$message_padded = $in;
@@ -66,7 +100,7 @@ function aes_cbc(&$in){
 		$message_padded = str_pad($message_padded,
         strlen($message_padded) + 16 - strlen($message_padded) % 16, "\xCC");
 	}
-	//echo bin2hex($message_padded)."\n";
+	//ecco(hexdump($message_padded));
 	$mac = aes_cmac($message_padded,$key);
 	return substr(openssl_encrypt($message_padded, $cipher, $key, $options=OPENSSL_RAW_DATA, $iv),0,-16).$mac;
 };
@@ -79,8 +113,21 @@ function get_date(){
 	$cnxdate = $cnxdate | (date('Y') - 1990) % 10;
 	$cnxdate = $cnxdate << 4;
 	$cnxdate = $cnxdate | date('n');
-	return hex2bin(dechex($cnxdate).date('is')); //!!!
+	return hex2bin(dechex($cnxdate).date('is'));
 };
+
+function emm_date(&$in){
+	$time = strtotime($in);
+	$cnxdate = floor((date('Y',$time) - 1990) / 10);
+	$cnxdate = $cnxdate << 5;
+	$cnxdate = $cnxdate | date('j',$time);
+ 	$cnxdate = $cnxdate << 4;
+	$cnxdate = $cnxdate | (date('Y',$time) - 1990) % 10;
+	$cnxdate = $cnxdate << 4;
+	$cnxdate = $cnxdate | date('n',$time);
+	return hex2bin(dechex($cnxdate));//.date('is'));
+};
+
 
 function get_ecm(&$chid,&$cw0,&$cw1,&$acc,&$cpnum){
 
@@ -92,12 +139,14 @@ function get_ecm(&$chid,&$cw0,&$cw1,&$acc,&$cpnum){
 	
 	if(date('n')%2){
 		$header .= hex2bin('21');
+		$key = $GLOBALS['key21'];
 	} else {
 		$header .= hex2bin('20');
+		$key = $GLOBALS['key20'];
 	};
-	
+	$GLOBALS['key'] = $key;
 	$ecm = hex2bin('2004').get_date().hex2bin('400f').$cw1.$cw0.hex2bin('2102').$chid.hex2bin('2204').$acc;
-	$ecm = $header.aes_cbc($ecm);
+	$ecm = $header.aes_cbc($ecm,$key);
 	return str_pad($ecm,254 - strlen($ecm),"\xFF");
 
 };
@@ -117,59 +166,47 @@ $data = unpack("Cver/ntyp/nlen/C*",$in);
 	        $val = $val << 8 & 0xFFFF;
 			$val = ($val | $data[$i]) & 0xFFFF;
 		if($val == 0x0e){
-        	//printf("channel-id: ");
             $i = $i+3;
             $chid = chr($data[$i]).chr($data[$i+1]);
-            //echo bin2hex($chid)." ";
             $i++;
         };
         if($val == 0x1){
-        	//printf("SuperCas-ID: ");
             $i = $i+3;
             $casid = chr($data[$i]).chr($data[$i+1]).chr($data[$i+2]).chr($data[$i+3]);
-            //echo bin2hex($casid)." ";
             $i+2;
         };
         if($val == 0xf){
-        	//printf("Stream-ID: ");
             $i = $i+3;
             $strid = chr($data[$i]).chr($data[$i+1]);
-            //echo bin2hex($strid)." ";
             $i++;
         }; 
         if($val == 0x19){
-        	//printf("ECM-ID: ");
             $i = $i+3;
             $ecmid = chr($data[$i]).chr($data[$i+1]);
-            //echo bin2hex($ecmid)." ";
             $i++;
         };        	
         if($val == 0x10){
-        	//printf("CP-duration: ");
             $i = $i+3;
             $dura = chr($data[$i]).chr($data[$i+1]);
-            //echo bin2hex($dura)." ";
             $i++;
         };
         if($val == 0x12){
-        	//printf("CP-num: ");
             $i = $i+3;
             $cpnum = chr($data[$i]).chr($data[$i+1]);
-            //echo bin2hex($cpnum)." ";
             $i++;
         };
 		if($val == 0x0d){
-        	//printf("Access-criteria: ");
 			$acc = '';
             $i++;
    	        $acclen = $data[$i] << 8 | $data[$i+1] & 0xFFFF;
-			//printf('%d ',$acclen);
             $i=$i+2;
+			for($j=0;$j<4-$acclen;$j++){
+				$acc .= chr(0);
+			};
             for($j=0;$j<$acclen;$j++){
             	$acc = $acc.chr($data[$i]);
                 $i++;
             };
-            //echo bin2hex($acc)." ";
             $i--;
         };
  		if($val == 0x14){
@@ -203,7 +240,7 @@ if($data['typ'] === $str_setup){
 if($data['typ'] == $cw_prov){
             ecco( "Channel-ID:    ".bin2hex($chid)."\n" );
 			ecco( "Stream-ID:     ".bin2hex($strid)."\n" );
-			ecco( "Acs-criteria:  ".bin2hex($acc)."\n" );
+			ecco( "Access-crit.:  ".bin2hex($acc)."\n" );
 			ecco( "CP-No:         ".bin2hex($cpnum)."\n" );
 			ecco( "CW0:           ".bin2hex($cw0)."\n" );
             ecco( "CW1:           ".bin2hex($cw1)."\n" );
@@ -222,4 +259,151 @@ if($data['typ'] == 0x0004){
 };
 	ecco( "\n" );
 };
+
+function emm_setup(&$socket){
+	ecco("Stream-Setup:\nSend:\n");
+	$out = hex2bin('020011001300030002000100010004000000000002000101');
+	ecco(hexdump($out));
+	socket_write($socket, $out, strlen($out));
+	$out = socket_read($socket, 2048);
+	ecco("Received:\n");
+	ecco(hexdump($out));
+	ecco("Send:\n");
+	$out = hex2bin('020111001F00030002000100040002000100010004000000000008000200000007000100');
+	ecco(hexdump($out));
+	socket_write($socket, $out, strlen($out));
+	$out = socket_read($socket, 2048);
+	ecco("Received:\n");
+	ecco(hexdump($out));
+	ecco("Send:\n");
+	$out = hex2bin('020117001A0003000200010004000200010001000400000000000600020064');
+	ecco(hexdump($out));
+	socket_write($socket, $out, strlen($out));
+	$out = socket_read($socket, 2048);
+	ecco("Received:\n");
+	ecco(hexdump($out));
+};
+
+
+function emm_unique(&$socket){
+	global $config;
+	$syskey = $GLOBALS['syskey'];
+
+	$servername = $config['servername']; 
+	$username = $config['dbusername'];
+	$password = $config['dbpass'];
+	$dbname = $config['dbname'];
+	$conn = mysqli_connect($servername, $username, $password, $dbname);
+	
+	if (!$conn) {
+		die("Connection failed: " . mysqli_connect_error());
+	}
+	
+	$sql = "
+		SELECT abo.ppua, abo.`bos`, abo.`eos`,abo.acc, providers.providername, cards.ppsa, providers.chid FROM neovision.providers 
+		join neovision.abo 
+		join neovision.cards
+        ON providers.chid = abo.chid and cards.ppua = abo.ppua and cards.deleted = 0;
+	";
+	
+	$result = $conn->query($sql);
+	while($row = $result->fetch_assoc()){
+		$ts = hex2bin(date('is'));
+		$ppua = hex2bin(predec($row['ppua']));
+		$bos = emm_date($row['bos']);
+		$eos = emm_date($row['eos']);
+		$acc = hex2bin(prehex($row['acc']));
+		$name = padname($row['providername']);
+		$ppsa = hex2bin(predec($row['ppsa']));
+		$chid = hex2bin($row['chid']);
+		
+		ecco("Unique EMM:\n");
+		ecco("PPUA:               ".prehex($row['ppua'])."\n");
+		ecco("Subscription start: ".$row['bos']."\n");
+		ecco("Subscription end:   ".$row['eos']."\n");
+		ecco("Access Criteria:    ".prehex($row['acc'])."\n");
+		ecco("Provider:           ".$row['providername']."\n");
+		ecco("Shared Address:     ".prehex($row['ppsa'])."\n");
+		ecco("Channel-ID:         ".$row['chid']."\n");
+		
+		$emm = $ts."\xa0\x00".$ppua."\xa0\x03".$bos.$eos."\xa0\x04".$acc."\xa0\x10".$name."\xa0\x02".$ppsa."\xa0\x01".$chid;
+		$key = aes_cmac($ppua,$syskey);
+		$emm = aes_cbc($emm,$key);
+		$emm = hex2bin('02021100DA0003000200010004000200010001000400000000000800020000000500BC475FFF100082704B000000').$ppua."\x70\x42\x64\x10".$emm;
+		$emm = str_pad($emm,325 - strlen($emm),"\xFF");
+		
+		ecco("------------------------------------------------\n");
+		ecco(hexdump($emm));
+		socket_write($socket, $emm, strlen($emm));
+	};
+	mysqli_close($conn);
+};
+
+function emm_shared(&$socket){
+	global $config;
+	$syskey = $GLOBALS['syskey'];
+	$servername = $config['servername']; 
+	$username = $config['dbusername'];
+	$password = $config['dbpass'];
+	$dbname = $config['dbname'];
+	$conn = mysqli_connect($servername, $username, $password, $dbname);
+	
+	if (!$conn) {
+		die("Connection failed: " . mysqli_connect_error());
+	}
+	
+	$sql = "
+		SELECT DISTINCT ppsa FROM neovision.cards JOIN neovision.abo WHERE cards.deleted = 0 AND cards.ppua = abo.ppua;
+	";
+	
+	$result = $conn->query($sql);
+	while($row = $result->fetch_assoc()){
+		$ts = hex2bin(date('is'));
+		$ppsa = hex2bin(predec($row['ppsa']));
+	
+		$emm = $ts."\xa0\x02".$ppsa."\xa0\x20".$GLOBALS['key20']."\xa0\x21".$GLOBALS['key21'];
+	
+		$key = aes_cmac($ppsa,$syskey);
+		$emm = aes_cbc($emm,$key);
+		$emm = hex2bin('02021100DA0003000200010004000200010001000400000000000800020000000500BC475FFF100082704B000000').$ppsa."\x70\x42\x64\x10".$emm;
+		$emm = str_pad($emm,325 - strlen($emm),"\xFF");
+		
+		ecco("Shared EMM:\n");
+		ecco("PPSA:   ".prehex($row['ppsa'])."\n");
+		ecco("Key20:  ".bin2hex($GLOBALS['key20'])."\n");
+		ecco("Key21:  ".bin2hex($GLOBALS['key21'])."\n");
+		
+		ecco("------------------------------------------------\n");
+		ecco(hexdump($emm));
+		socket_write($socket, $emm, strlen($emm));
+	};
+	mysqli_close($conn);
+};
+
+function dbclean(){
+	global $config;
+	$servername = $config['servername']; 
+	$username = $config['dbusername'];
+	$password = $config['dbpass'];
+	$dbname = $config['dbname'];
+	$conn = mysqli_connect($servername, $username, $password, $dbname);
+	
+	if (!$conn) {
+		die("Connection failed: " . mysqli_connect_error());
+	}
+	
+	$sql = "
+		DELETE FROM neovision.abo WHERE abo.eos < (NOW() - INTERVAL 2 MONTH);
+	";
+	$result = $conn->query($sql);
+
+	$sql = "
+		UPDATE neovision.ecmg_keys SET ecmg_keys.ecmkey = md5(rand()*1001), ecmg_keys.modified = (NOW() + INTERVAL 1 MONTH)
+        WHERE ecmg_keys.modified < (NOW() - INTERVAL 1 MONTH);
+		";
+	$result = $conn->query($sql);	
+	
+	mysqli_close($conn);
+};
+
 ?>
